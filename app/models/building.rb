0c16a2a0d755d4d60c411d778265aeb2b838e32a
Sam Bauch <sbauch@gmail.com>
2011-12-23 11:54:20 -0500

    nogeo1

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,6 +1,6 @@
 class Building < ActiveRecord::Base
 attr_accessible :lat, :lon
-set_rgeo_factory_for_column(:latlon, RGeo::Geographic.spherical_factory(:srid => 4326))
+#set_rgeo_factory_for_column(:latlon, RGeo::Geographic.spherical_factory(:srid => 4326))
 has_many :incidents, :class_name => "Incident", :foreign_key => 'building_address', :primary_key => 'address',
 end
 

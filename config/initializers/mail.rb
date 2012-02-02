500d6971c95ffe1a32124206c9654741d283bd51
Sam Bauch <sbauch@gmail.com>
2012-02-02 00:44:26 -0500

    get money1

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -4,6 +4,6 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['app2181483@heroku.com'],
   :password       => ENV['@##csa3141598'],
-  :domain         => 'heroku.com'
+  :domain         => 'uhpartments.com'
 }
 ActionMailer::Base.delivery_method = :smtp

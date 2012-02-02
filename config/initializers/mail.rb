7be097e1d66189de14672d6721010bf3fd35e756
Sam Bauch <sbauch@gmail.com>
2012-02-02 00:46:32 -0500

    get money2

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['app2181483@heroku.com'],
-  :password       => ENV['@##csa3141598'],
-  :domain         => 'uhpartments.com'
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
